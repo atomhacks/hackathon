90601da63677bbdaceb2ff88aee25cfd10a26f3c
Jack Cook <jack@jackcook.nyc>
2015-04-04 09:45:57 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+(function($) {
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    var id = $(this).attr('href');
+    var $id = $(id);
+    if ($id.length === 0) {
+      return;
+    }
+
+    e.preventDefault();
+    var pos = $(id).offset().top;
+    $('body, html').animate({scrollTop: pos});
+  });
+})(jQuery);
