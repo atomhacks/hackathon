8d3435681fa280ad05a92d6fd593c32b933a65c6
Jack Cook <jack@jackcook.nyc>
2015-04-08 19:43:00 -0600

    Finished starter mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (function($) {
   $(document).on('click', '#hamburger', function(e) {
-    
+    if ($('nav').hasClass('visible')) {
+      $('nav').removeClass('visible');
+    } else {
+      $('nav').addClass('visible')
+    }
   });
 })(jQuery);
