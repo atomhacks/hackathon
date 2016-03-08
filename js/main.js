3bf666396d80e99654d7c96628e781d23d5293b4
Jack Cook <hello@jackcook.nyc>
2016-03-08 17:41:14 -0500

    New website for 2016

--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $(document).on('click', '#hamburger', function(e) {
-    if ($('nav').hasClass('visible')) {
-      $('nav').removeClass('visible');
-    } else {
-      $('nav').addClass('visible')
-    }
-  });
-})(jQuery);
